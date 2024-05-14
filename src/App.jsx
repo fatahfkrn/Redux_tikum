2d4a56884a9ed9efade7266f9b3e5cecacecac00
fatahfkrn <fatahfkrn@gmail.com>
2024-05-14 19:59:13 +0700

    push tes

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css';
+import Home from "./Home";
+import User from "./User";
+import Redux from "./Redux";
+import Reduxuser from "./Reduxuser";
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={ <Home /> } />
+        <Route path="/redux" element={ <Redux /> } />
+        <Route path="/reduxuser" element={ <Reduxuser /> } />
+        <Route path="/user" element={ <User /> } />
+      </Routes>
+    </>
+  )
+};
+
+export default App
---
+++ src/redux/app.js
@@ -0,0 +1,10 @@
+import Counter from "./slices/CounterComponent";
+
+function App() {
+    return (
+        <main className="App">
+            <Counter />
+        </main>
+
+    );
+}
